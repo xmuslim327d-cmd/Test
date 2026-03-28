859f819bf62d65fabb25487bfcf1c2b083d8ad8e
xmuslim327d-cmd <xmuslim327d@gmail.com>
2026-03-29 01:42:04 +0300

    Delete script 2.js

--- script 2.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    let nav = document.getElementById("nav");
-    if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-    }
-}
